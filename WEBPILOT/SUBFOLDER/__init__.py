7bdd90deb28f6a15b12826e0c4d5bb8d094d770b
Nishchal Soni <s.nishchal27.soni@gmail.com>
2026-05-28 19:28:49 +0530

    Delete WEBPILOT directory

--- WEBPILOT/SUBFOLDER/__init__.py
+++
@@ -1,6 +0,0 @@
-"""WebPilot — Autonomous Web Agent"""
-from .agent import WebPilotAgent
-from .browser import BrowserController
-
-__all__ = ["WebPilotAgent", "BrowserController"]
-__version__ = "1.0.0"
